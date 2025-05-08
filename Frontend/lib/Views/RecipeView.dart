ff346ba0d13cce1f0dbbda539cdbb5729879c7a9
이건민 <djrlee1@naver.com>
2025-05-09 01:35:16 +0900

    Feat - 레시피, 즐겨찾기 UI 프로토타입

--- Frontend/lib/Views/RecipeView.dart
+++ Frontend/lib/Views/RecipeView.dart
@@ -1,5 +1,8 @@
+import 'package:Frontend/Models/RecipeModel.dart';
 import 'package:flutter/material.dart';
 import 'package:Frontend/Views/MainFrameView.dart';
+
+import '../Models/IngredientModel.dart';
 
 class RecipeView extends StatefulWidget {
   const RecipeView({Key? key}) : super(key: key);
@@ -12,6 +15,7 @@ class RecipePage extends State<RecipeView> {
   String _searchQuery = '';
   TextEditingController _searchController = TextEditingController();
   late ScrollController _scrollController;
+  RecipesModel recipeStorage = RecipesModel();
 
   RecipePage(){
     super.initState();
@@ -24,10 +28,32 @@ class RecipePage extends State<RecipeView> {
     _scrollController.dispose();
   }
 
+  List<String> getIngredientsType(){
+    List<String> str = [];
+    str.add('');
+    List<RecipeModel>? recipes = recipeStorage.recipes;
+
+    for(RecipeModel recipe in recipes!){
+      String temp = '';
+      for(Ingredient ingredient in recipe.ingredients!){
+        temp += ingredient.ingredientName!;
+        temp += ', ';
+      }
+      str.add(temp);
+    }
+
+    return str;
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+    recipeStorage.makeRecipesList(num: 10);
+
+    //올바르게 작동하려면 레시피를 꾸준히 업데이트 하는 방식으로 수정해야함
+    List<RecipeModel>? recipes = recipeStorage.recipes;
+    List<String> ingredientsTypes = getIngredientsType();
 
     return Scaffold(
       // 냉장고 선택 페이지 UI
@@ -50,7 +76,7 @@ class RecipePage extends State<RecipeView> {
                         controller: _scrollController,
                         children: <Widget>[
                           // 실제론 DB에서 랜덤으로 추천돌려서 레시피로 띄워줌
-                          for(int i = 0; i < 10; i++)
+                          for(RecipeModel recipe in recipes!)
                             Container(
                               margin: EdgeInsets.all(20),
                               height: screenHeight * 0.18,
@@ -61,6 +87,7 @@ class RecipePage extends State<RecipeView> {
                               child: Row(
                                 mainAxisAlignment: MainAxisAlignment.start,
                                 children: <Widget>[
+                                  // 레시피 이미지
                                   Container(
                                     margin: EdgeInsets.fromLTRB((screenWidth - 40) * 0.03, 0, (screenWidth - 40) * 0.04, 0),
                                     width: (screenWidth - 40) * 0.3,
@@ -70,15 +97,95 @@ class RecipePage extends State<RecipeView> {
                                       borderRadius: BorderRadius.circular(30),
                                     ),
                                   ),
+                                  // 레시피 설명
                                   Container(
                                     margin: EdgeInsets.fromLTRB(0, 0, (screenWidth - 40) * 0.03, 0),
                                     width: (screenWidth - 40) * 0.6,
                                     height: (screenWidth - 40) * 0.3,
                                     decoration: BoxDecoration(
-                                      color: Colors.white,
                                       borderRadius: BorderRadius.circular(30),
                                     ),
-                                  ),
+                                    child: FilledButton(
+                                      onPressed: ()=>{
+                                        setState(() {
+                                          // 이 부분에 모달 창 띄워줘야 함
+
+                                        })
+                                      },
+                                      style: FilledButton.styleFrom(
+                                        backgroundColor: Colors.white,
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                                        padding: EdgeInsets.zero,
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: <Widget>[
+                                          Flexible(
+                                            flex: 2,
+                                            fit: FlexFit.tight,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                color: Colors.white,
+                                                borderRadius: BorderRadius.circular(30),
+                                              ),
+                                              child: Column(
+                                                mainAxisAlignment: MainAxisAlignment.start,
+                                                children: <Widget>[
+                                                  Flexible(
+                                                    flex: 1,
+                                                    fit: FlexFit.tight,
+                                                    child: Row(
+                                                      mainAxisAlignment: MainAxisAlignment.start,
+                                                      children: <Widget>[
+                                                        SizedBox(width: 10),
+                                                        Text(recipe.recipeName!,
+                                                          style: TextStyle(
+                                                              color: Colors.black,
+                                                              fontWeight: FontWeight.bold
+                                                          )
+                                                        ),
+                                                      ]
+                                                    )
+                                                  ),
+                                                  Flexible(
+                                                    flex: 4,
+                                                    fit: FlexFit.tight,
+                                                    child: Column(
+                                                      mainAxisAlignment: MainAxisAlignment.start,
+                                                      children: <Widget>[
+                                                        // 컨테이너에서 텍스트 위치 설정하는 테크닉, 잘 기억해두길...
+                                                        Row(
+                                                          mainAxisAlignment: MainAxisAlignment.start,
+                                                          children: <Widget>[
+                                                            SizedBox(width: 10),
+                                                            Flexible(
+                                                                child: Text(ingredientsTypes[recipe.recipeNum!],
+                                                                    style: TextStyle(color: Colors.black)
+                                                                )
+                                                            )
+                                                          ]
+                                                        )
+                                                      ],
+                                                    )
+                                                  ),
+                                                ],
+                                              )
+                                            ),
+                                          ),
+                                          Flexible(
+                                            flex: 1,
+                                            fit: FlexFit.tight,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                color: Colors.white,
+                                                borderRadius: BorderRadius.circular(30),
+                                              ),
+                                            ),
+                                          ),
+                                        ]
+                                      ),
+                                      ),
+                                    )
                                 ],
                               )
                             )
