513af91ff7349d826195876e1731d2d4166d835f
Myeonghoonjin <audgns1947@dgu.ac.kr>
2025-05-09 23:21:01 +0900

    Frontend Added

---
+++ Frontend/lib/Models/LoginModel.dart
@@ -0,0 +1,24 @@
+// No Statement Management
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:Frontend/Abstracts/kakaoLoginAbstract.dart';
+
+class LoginModel{
+  final SocialLogin _socialLogin;
+  bool isLogined = false;
+  User? user;     //Nullable
+
+  LoginModel(this._socialLogin);
+
+  Future login() async {
+    isLogined = await _socialLogin.login();
+    if (isLogined) {
+      user = await UserApi.instance.me();
+    }
+  }
+
+  Future logout() async {
+    await _socialLogin.logout();
+    isLogined = false;
+    user = null;
+  }
+}
