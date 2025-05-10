39c96823b4e6ce4cdd1ff99cae8cca74c9c91962
이건민 <djrlee1@naver.com>
2025-05-10 19:21:26 +0900

    Test : BE, FE 토큰 발급 통신 테스트

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -3,35 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:Frontend/Views/LoginView.dart';
 import 'package:Frontend/Views/MainFrameView.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-import 'package:mysql_client/mysql_client.dart';
 
 void main() {
   KakaoSdk.init(nativeAppKey: nativeAppKey);
   runApp(const MyApp());
-}
-
-Future<void> dbConnector() async {
-  print("Connecting to mysql server...");
-
-  // MySQL 접속 설정
-  final conn = await MySQLConnection.createConnection(
-    host: '127.0.0.1',
-    port: 0000,
-    userName: 'userName',
-    password: 'password',
-    databaseName: 'testdb', // optional
-  );
-
-  // 연결 대기
-  try {
-    print("Connecting...");
-    await conn.connect();
-  }
-  catch(e){
-    print('DB 연결 실패. 에러 로그${e}');
-  }
-  // 종료 대기
-  await conn.close();
 }
 
 class MyApp extends StatelessWidget {
