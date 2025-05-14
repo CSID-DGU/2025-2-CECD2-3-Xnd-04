5ff585272679add209b44c8c5c87f9d7dfd719b4
wjdgus000000000 <wjdgus000000000@gmail.com>
2025-05-14 11:45:29 +0900

    feat: 레시피 상세 조회

--- Backend/XndApp/urls.py
+++ Backend/XndApp/urls.py
@@ -1,8 +1,8 @@
 # XndApp/urls.py
 from django.urls import path
-from XndApp.Views.RecipeViews import RecipeView
+from XndApp.Views.RecipeViews import RecipeView, RecipeDetailView
 
 urlpatterns = [
     path('api/recipes/', RecipeView.as_view(), name='recipe-list'),
-    path('api/recipes/<int:recipe_id>/', RecipeView.as_view(), name='recipe-detail'),
+    path('api/recipes/<int:recipe_id>/', RecipeDetailView.as_view(), name='recipe-detail'),
 ]
