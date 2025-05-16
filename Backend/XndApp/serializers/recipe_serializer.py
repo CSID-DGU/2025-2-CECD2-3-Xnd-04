76b8471ea592af305c633bd7852a11d892d11c14
Myeonghoonjin <audgns1947@dgu.ac.kr>
2025-05-16 16:00:26 +0900

    merged to hyeon

--- Backend/XndApp/serializers/recipe_serializer.py
+++ Backend/XndApp/serializers/recipe_serializer.py
@@ -4,7 +4,12 @@ from ..Models.recipes import Recipes
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipes
-        fields = ['food_name', 'recipe_image', 'steps', 'serving_size', 'cooking_time', 'cooking_level']
+        fields = ['recipe_id', 'food_name', 'recipe_image', 'ingredient_all']
 
 
+class RecipeDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipes
+        fields = ['food_name', 'recipe_image', 'ingredient_all', 'serving_size', 'cooking_time', 'cooking_level', 'steps']
 
+        # 식재료가 장바구니에 담겼는지 여부 추가
