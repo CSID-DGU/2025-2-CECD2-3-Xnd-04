825c58cf3f2c79ebe43a0a00cd76098e4feb5e41
이건민 <djrlee1@naver.com>
2025-05-21 12:07:13 +0900

    UI Done

--- Frontend/lib/Views/IngredientsInfoView.dart
+++ Frontend/lib/Views/IngredientsInfoView.dart
@@ -15,6 +15,57 @@ class IngredientsInfoView extends StatelessWidget{
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
+    Container recipeInfoDescription(String rName, String rDesc){
+      return Container(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: (screenWidth - 80) * 0.5,
+                    height: screenHeight * 0.04,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(rName, style: TextStyle(fontSize: screenWidth * 0.04))
+                    )
+                  ),
+                  Container(
+                    width: (screenWidth - 80) * 0.5,
+                    height: screenHeight * 0.04,
+                    child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Icon(Icons.star_border, size: screenWidth * 0.04,)
+                    )
+                  )
+                ],
+              ),
+              Container(
+                height: 1.5,
+                child: Divider(
+                  color: Colors.grey[300]
+                )
+              ),
+              Container(
+                height: screenHeight * 0.02,
+                color: Colors.cyanAccent,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Icon(Icons.aspect_ratio, size: screenWidth * 0.03,)
+                )
+              ),
+              Container(
+                height: screenHeight * 0.38 - 152,
+                width: screenWidth - 80,
+                child: Text(rDesc, style: TextStyle(fontSize: screenWidth * 0.03, fontWeight: FontWeight.bold), textAlign: TextAlign.left,)
+              )
+            ],
+          )
+      );
+    }
+
     return Scaffold(
       appBar: basicBar(),
       backgroundColor: Colors.white,
@@ -65,7 +116,7 @@ class IngredientsInfoView extends StatelessWidget{
             Container(
               width: screenWidth - 60,
               height: screenHeight * 0.07,
-              margin: EdgeInsets.fromLTRB(30, 30, 30, 25),
+              margin: EdgeInsets.fromLTRB(30, 20, 30, 20),
               padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
               decoration: BoxDecoration(
                 color: Colors.grey[100],
@@ -95,7 +146,7 @@ class IngredientsInfoView extends StatelessWidget{
             Container(
               width: screenWidth - 60,
               height: screenHeight * 0.07,
-              margin: EdgeInsets.fromLTRB(30, 0, 30, 25),
+              margin: EdgeInsets.fromLTRB(30, 0, 30, 20),
               padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
               decoration: BoxDecoration(
                   color: Colors.grey[100],
@@ -122,7 +173,7 @@ class IngredientsInfoView extends StatelessWidget{
               )
             ),
             Container(
-              height: screenHeight * 0.44 - 105,
+              height: screenHeight * 0.44 - 85,
               width: screenWidth - 60,
               child: DefaultTabController(
                 length: 3,
@@ -134,32 +185,24 @@ class IngredientsInfoView extends StatelessWidget{
                       topPadding: 0,
                       alignment: TabAxisAlignment.start,
                       tabs: [
-                        TabItem(color: Colors.red, title: Text('Home', style: TextStyle(fontSize: screenWidth * 0.03),)),
-                        TabItem(color: Colors.green, title: Text('Favorite', style: TextStyle(fontSize: screenWidth * 0.03),)),
-                        TabItem(color: Colors.orange, title: Text('Search', style: TextStyle(fontSize: screenWidth * 0.03),)),
+                        TabItem(color: Colors.red, title: Text('소고기 미역국', style: TextStyle(fontSize: screenWidth * 0.03),)),
+                        TabItem(color: Colors.blue, title: Text('스테이크', style: TextStyle(fontSize: screenWidth * 0.03),)),
+                        TabItem(color: Colors.yellow, title: Text('카레', style: TextStyle(fontSize: screenWidth * 0.03),)),
                       ]
                     ),
                     Container(
-                      height: screenHeight * 0.2,
+                      height: screenHeight * 0.44 - 140,
                       width : screenWidth - 60,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(10)
+                      ),
                       child: TabBarView(
                         children: <Widget>
                         [
-                          Container(
-                            width: screenWidth - 60,
-                            height: screenHeight * 0.15,
-                            color: Colors.red,
-                          ),
-                          Container(
-                            width: screenWidth - 60,
-                            height: screenHeight * 0.15,
-                            color: Colors.green,
-                          ),
-                          Container(
-                            width: screenWidth - 60,
-                            height: screenHeight * 0.15,
-                            color: Colors.orange,
-                          ),
+                          recipeInfoDescription('소고기 미역국', '소고기 미역국 레시피 입니다.'),
+                          recipeInfoDescription('스테이크', '스테이크 레시피 입니다.'),
+                          recipeInfoDescription('카레', '카레 레시피 입니다.'),
                         ]
                       )
                     )
