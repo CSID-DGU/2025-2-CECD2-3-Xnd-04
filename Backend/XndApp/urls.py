de1ee43dbc8511d9c033930bdfa5a9805b35077e
진명훈 <jinmyeonghun@jinmyeonghun-ui-MacBookAir.local>
2025-05-23 17:48:02 +0900

    Fix : urls.py 수정

--- Backend/XndApp/urls.py
+++ Backend/XndApp/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from XndApp.Views.RecipeViews import RecipeView, RecipeDetailView
 from XndApp.Views.loginViews import KakaoLoginView
 from XndApp.Views.loginViews import NaverLoginView
-from XndApp.Views.createFridgeViews import CreateFridgeView
 from XndApp.Views.fridgesViews import FridgeViews
 from XndApp.Views.fridgeDetailViews import FridgeDetailView
 from XndApp.Views.IngredientViews import IngredientView
-# from XndApp.Views.CartViews import CartListView, CartManageView
+from XndApp.Views.CartViews import CartListView, CartManageView
 from XndApp.Views.savedRecipesViews import SavedRecipesView,SavedRecipeDetailView
 
 
@@ -17,7 +16,7 @@ urlpatterns = [
     path('api/auth/naver-login/',NaverLoginView.as_view(),name='naver_login'), # 네이버 로그인
 
     # 냉장고
-    path('api/fridge/create/',CreateFridgeView.as_view(),name='create_fridge'), # 냉장고 생성
+    path('api/fridge/create/',FridgeViews.as_view(),name='create_fridge'), # 냉장고 생성
     path('api/fridge/',FridgeViews.as_view(),name='fridges'), # 냉장고 정보 조회
     path('api/fridge/<int:fridge_id>/',FridgeDetailView.as_view(),name='fridgeDetails'), # 냉장고 내부 조회
     path('api/ingredients/<int:id>/', IngredientView.as_view(), name='ingredient-detail'),  # 냉장고 속 재료 하나 선택했을 때 정보 조회
@@ -27,9 +26,9 @@ urlpatterns = [
     path('api/recipes/<int:recipe_id>/', RecipeDetailView.as_view(), name='recipe-detail'),  # 레시피 상세 조회
 
     # 장바구니
-    # path('api/cart/', CartListView.as_view(), name='cart-list'), # 장바구니 목록 조회
-    # path('api/cart/add/', CartManageView.as_view(), name='cart-add'), # 장바구니에 추가
-    # path('api/cart/<int:cart_id>/', CartManageView.as_view(), name='cart-manage'), # 장바구니 수량 + - x (삭제)
+    path('api/cart/', CartListView.as_view(), name='cart-list'), # 장바구니 목록 조회
+    path('api/cart/add/', CartManageView.as_view(), name='cart-add'), # 장바구니에 추가
+    path('api/cart/<int:cart_id>/', CartManageView.as_view(), name='cart-manage'), # 장바구니 수량 + - x (삭제)
 
     #즐겨찾기(레시피 저장)
     path('api/savedRecipe/add',SavedRecipesView.as_view(),name='saveRecipe'),
