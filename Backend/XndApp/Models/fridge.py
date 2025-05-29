8ae5001a2a493dca19673be344a37df05f198c2d
이건민 <djrlee1@naver.com>
2025-05-30 03:42:48 +0900

    다시

--- Backend/XndApp/Models/fridge.py
+++ Backend/XndApp/Models/fridge.py
@@ -11,9 +11,9 @@ class Fridge(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,default=111)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    # Admin페이지에서 냉장고를 구분하기 위한 방법
-    def __str__(self):
-        return f"{self.user.get_full_name()}의 냉장고 ({self.model_label})"
+    # # Admin페이지에서 냉장고를 구분하기 위한 방법
+    # def __str__(self):
+    #     return f"{self.user.get_full_name()}의 냉장고 ({self.model_label})"
 # 메타데이터
 class Meta:
     db_table = 'fridge'  
