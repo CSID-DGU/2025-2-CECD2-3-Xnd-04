28dada3bcaaa6e0f78f972331ee901e3bb6d3847
Myeonghoonjin <audgns1947@dgu.ac.kr>
2025-06-01 01:29:31 +0900

    Feat : 레시피 상세 보기 수정 및 serializer 설정 추가(미완성version)

--- Backend/XndApp/serializers/fridge_ingredients_serializer.py
+++ Backend/XndApp/serializers/fridge_ingredients_serializer.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from ..Models.fridgeIngredients import FridgeIngredients
+from XndApp.serializers.Ingredient_serializers import IngredientSerializer
 
 class FridgeIngredientsSerializer(serializers.ModelSerializer):
+    ingredient = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='name'
+    )
     class Meta:
         model = FridgeIngredients
         fields = [
             'id',
-            'ingredient_name',
+            'ingredient',
             'layer',
             'stored_at',
             'storable_due',
