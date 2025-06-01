9df7921d6bb9b8c69672a4aa81535f3de649b288
wjdgus000000000 <wjdgus000000000@gmail.com>
2025-06-01 18:10:52 +0900

    백엔드 최종 & DB 저장 코드

--- Backend/XndApp/Views/IngredientViews.py
+++ Backend/XndApp/Views/IngredientViews.py
@@ -4,18 +4,14 @@ from rest_framework import status
 from XndApp.Models import FridgeIngredients
 from XndApp.Models import Fridge
 from django.shortcuts import get_object_or_404
-from ..serializers.Ingredient_serializers import IngredientSerializer
+from ..serializers.fridge_ingredients_serializer import FridgeIngredientsSerializer
 from rest_framework.permissions import AllowAny # 테스트용
 
 class IngredientView(APIView):
-    def get(self, request, fridge_id, ingredient_id):  # 매개변수 수정
+    def get(self, request, fridge_id, ingredient_id):
         user_id = request.user.user_id
-
-        # 냉장고 소유권 확인
         fridge = get_object_or_404(Fridge, fridge_id=fridge_id, user_id=user_id)
+        ingredient = get_object_or_404(FridgeIngredients, id=ingredient_id, fridge_id=fridge_id)
 
-        # 해당 냉장고의 특정 재료 가져오기
-        ingredient = get_object_or_404(FridgeIngredients, ingredient_id=ingredient_id, fridge_id=fridge_id)
-
-        serializer = IngredientSerializer(ingredient)
+        serializer = FridgeIngredientsSerializer(ingredient)
         return Response(serializer.data, status=status.HTTP_200_OK)
