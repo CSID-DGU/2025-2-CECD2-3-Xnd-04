7ee666edaede57975eb1c7889476675a4b83ce8b
wjdgus000000000 <wjdgus000000000@gmail.com>
2025-10-15 15:51:22 +0900

    feat: Implement Ingredient Update API (PATCH)

--- Backend/XndApp/Views/IngredientViews.py
+++ Backend/XndApp/Views/IngredientViews.py
@@ -15,3 +15,31 @@ class IngredientView(APIView):
 
         serializer = FridgeIngredientsSerializer(ingredient)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, fridge_id, ingredient_id):
+        user_id = request.user.user_id
+
+        get_object_or_404(Fridge, fridge_id=fridge_id, user_id=user_id)
+
+        # 수정할 식재료 인스턴스 조회
+        ingredient_instance = get_object_or_404(
+            FridgeIngredients,
+            id=ingredient_id,
+            fridge_id=fridge_id
+        )
+
+        serializer = FridgeIngredientsSerializer(
+            ingredient_instance,
+            data=request.data,
+            partial=True # 요청 보낸 부분만 수정 (식재료명/유통기한)
+        )
+
+        if serializer.is_valid():
+            instance = serializer.save()
+
+            return Response(
+                FridgeIngredientsSerializer(instance).data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
