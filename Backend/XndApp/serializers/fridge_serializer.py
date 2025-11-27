02e2a7521f00a214356a67c948a225393f084039
wjdgus000000000 <wjdgus000000000@gmail.com>
2025-11-27 23:00:31 +0900

    backend integration with embedded system

--- Backend/XndApp/serializers/fridge_serializer.py
+++ Backend/XndApp/serializers/fridge_serializer.py
@@ -4,5 +4,5 @@ from ..Models.fridge import Fridge
 class FridgeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fridge
-        fields = ['model_label','layer_count','user','created_at']
+        fields = ['fridge_id','model_label','layer_count','user','created_at']
         read_only_fields=['user','created_at']
