f148dfff1626db516c2bf47929ec0b3995d61918
Ubuntu <ubuntu@ip-172-31-37-57.ap-northeast-2.compute.internal>
2025-12-01 13:21:57 +0000

    Fix - Model not loaded Error

--- Backend/Xnd/settings.py
+++ Backend/Xnd/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'XndApp',
+    'XndApp.apps.SrmappConfig',
     'rest_framework',
     'rest_framework_simplejwt',
     'storages',  # django-storages 앱 등록
